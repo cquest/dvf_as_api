ebfac377fdcd222e390338fbb61e43c1256ac071
cquest <cquest@openstreetmap.fr>
2019-11-01 12:30:57 +0100

    mise à jour URL et années

--- dvf_download.sh
+++ dvf_download.sh
@@ -1,10 +1,12 @@
 #! /bin/bash
 
+MILLESIME=$1
+
 mkdir -p data
 cd data
 # Téléchargement des fichiers DVF
-for ANNEE in $(seq 2014 2018)
+for ANNEE in $(seq 2014 2019)
 do
-  wget -nc http://data.cquest.org/dgfip_dvf/valeursfoncieres-$ANNEE.txt.gz
-  wget -nc https://cadastre.data.gouv.fr/data/hackathon-dgfip-dvf/contrib/etalab-csv/$ANNEE/full.csv.gz -O $ANNEE-full.csv.gz
+  wget -N http://data.cquest.org/dgfip_dvf/$MILLESIME/valeursfoncieres-$ANNEE.txt.gz
+  wget -N https://cadastre.data.gouv.fr/data/etalab-dvf/$MILLESIME/csv/$ANNEE/full.csv.gz -O $ANNEE-full.csv.gz
 done
